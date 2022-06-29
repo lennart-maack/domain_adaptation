713d676fa285293e3796560bdb2a3ccf13ef60fd
Lennart <LennartMa.19@gmail.com>
2022-06-29 17:04:12 +0200

    Add UNet architecture with ResNet18 backbone

--- segmentation_example/segmentation_model.py
+++ segmentation_example/segmentation_model.py
@@ -7,6 +7,7 @@ from metrics import Dice_Coefficient
 
 from torchvision import models
 from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+import torchvision.transforms.functional as TF
 
 
 
@@ -80,27 +81,87 @@ class UNet(nn.Module):
         return self.layers[-1](xi[-1])
 
 
+class UNET_(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, features=[32, 64, 128, 256], bilinear=False):
+        super(UNET_, self).__init__()
+        
+        self.downs = nn.ModuleList()
+        self.ups = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # U-Net Down part
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # U-Net Up part
+        for feature in reversed(features):
+            # in the original paper they used bilinear upsampling, but ConvTransposed2d should work better
+
+            if bilinear:
+                self.ups.append(nn.Sequential(
+                    nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True),
+                    nn.Conv2d(feature*2, feature, kernel_size=1),
+                    )
+                )
+            else:
+                self.ups.append(
+                    nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2)
+                )
+            self.ups.append(DoubleConv(features*2, features))
+
+        # creates the final feature map (latent space) in the U-Net
+        self.bottlneck = DoubleConv(feature[-1], features[-1]*2)
+
+        # output layer
+        self.output_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottlneck(x)
+
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim = 1)
+            x = self.ups[idx+1](concat_skip)
+
+        return self.output_conv(x)
+
 class DoubleConv(nn.Module):
     """Double Convolution and BN and ReLU (3x3 conv -> BN -> ReLU) ** 2.
     >>> DoubleConv(4, 4)  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
     DoubleConv(
-      (net): Sequential(...)
+      (conv): Sequential(...)
     )
     """
 
     def __init__(self, in_ch: int, out_ch: int):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
-        return self.net(x)
+        return self.conv(x)
 
 
 class Down(nn.Module):
@@ -163,6 +224,86 @@ class Up(nn.Module):
         return self.conv(x)
 
 
+def convrelu(in_channels, out_channels, kernel, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+        nn.ReLU(inplace=True),
+    )
+
+
+class UNET_ResNet18_backbone(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+
+        self.base_model = models.resnet18(pretrained=True)
+        state_dict = torch.hub.load_state_dict_from_url('https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth')
+        # self.base_model.load_state_dict(state_dict)
+        self.base_layers = list(self.base_model.children())
+
+        self.layer0 = nn.Sequential(*self.base_layers[:3]) #Sequential((0): Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)(1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)(2): ReLU(inplace=True))
+        self.layer0_1x1 = convrelu(64, 64, 1, 0)
+        self.layer1 = nn.Sequential(*self.base_layers[3:5])
+        self.layer1_1x1 = convrelu(64, 64, 1, 0)
+        self.layer2 = self.base_layers[5]
+        self.layer2_1x1 = convrelu(128, 128, 1, 0)
+        self.layer3 = self.base_layers[6]
+        self.layer3_1x1 = convrelu(256, 256, 1, 0)
+        self.layer4 = self.base_layers[7]
+        self.layer4_1x1 = convrelu(512, 512, 1, 0)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv_up3 = convrelu(256 + 512, 512, 3, 1)
+        self.conv_up2 = convrelu(128 + 512, 256, 3, 1)
+        self.conv_up1 = convrelu(64 + 256, 256, 3, 1)
+        self.conv_up0 = convrelu(64 + 256, 128, 3, 1)
+
+        self.conv_original_size0 = convrelu(3, 64, 3, 1)
+        self.conv_original_size1 = convrelu(64, 64, 3, 1)
+        self.conv_original_size2 = convrelu(64 + 128, 64, 3, 1)
+
+        self.conv_last = nn.Conv2d(64, n_classes, 1)
+
+
+        def forward(self, input):
+            x_original = self.conv_original_size0(input)
+            x_original = self.conv_original_size1(x_original)
+
+            layer0 = self.layer0(input)
+            layer1 = self.layer1(layer0)
+            layer2 = self.layer2(layer1)
+            layer3 = self.layer3(layer2)
+            layer4 = self.layer4(layer3)
+
+            layer4 = self.layer4_1x1(layer4)
+            x = self.upsample(layer4)
+            layer3 = self.layer3_1x1(layer3)
+            x = torch.cat([x, layer3], dim=1)
+            x = self.conv_up3(x)
+
+            x = self.upsample(x)
+            layer2 = self.layer2_1x1(layer2)
+            x = torch.cat([x, layer2], dim=1)
+            x = self.conv_up2(x)
+
+            x = self.upsample(x)
+            layer1 = self.layer1_1x1(layer1)
+            x = torch.cat([x, layer1], dim=1)
+            x = self.conv_up1(x)
+
+            x = self.upsample(x)
+            layer0 = self.layer0_1x1(layer0)
+            x = torch.cat([x, layer0], dim=1)
+            x = self.conv_up0(x)
+
+            x = self.upsample(x)
+            x = torch.cat([x, x_original], dim=1)
+            x = self.conv_original_size2(x)
+
+            out = self.conv_last(x)
+
+            return out
+
 class SegModel(LightningModule):
     """Semantic Segmentation Module.
     This is a basic semantic segmentation module implemented with Lightning.
@@ -211,6 +352,9 @@ class SegModel(LightningModule):
                 num_classes=self.num_classes, num_layers=self.num_layers, features_start=self.features_start, bilinear=self.bilinear
             )
         
+        if model_name == "unet_resnet_backbone":
+            self.net = UNET_ResNet18_backbone(num_classes)
+
         elif model_name == "deeplabv3":
             self.net = createDeepLabv3()
 
