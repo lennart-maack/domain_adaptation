c09de4746f72ebbf5323c30fe096386feb3fa539
Lennart <LennartMa.19@gmail.com>
2022-09-05 10:05:22 +0200

    Create multiple model prediction

--- trainer_FDA.py
+++ trainer_FDA.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser, Namespace
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-from models.FDA import FDA_first_train, UNet_baseline
+from models.FDA import FDA_first_train, FDA_self_supervised, UNet_baseline
 
 from utils.data import DataModuleSegmentation
 
@@ -12,14 +12,21 @@ def main_FDA(hparams: Namespace):
     
     if hparams.model_type == "FDA":
         model = FDA_first_train(LB=hparams.LB)
+        dm = DataModuleSegmentation(path_to_train_source=hparams.source_data_path, path_to_train_target=hparams.target_data_path,
+            path_to_test=hparams.test_data_path, domain_adaptation=True, batch_size=hparams.batch_size, load_size=hparams.load_size)
+    
+    elif hparams.model_type == "FDA_self_supervised":
+        model = FDA_self_supervised(LB=hparams.LB)
+        dm = DataModuleSegmentation(path_to_train_source=hparams.source_data_path, path_to_train_target=hparams.target_data_path,
+            path_to_test=hparams.test_data_path, domain_adaptation=True, pseudo_labels=True, batch_size=hparams.batch_size, load_size=hparams.load_size)
+    
     elif hparams.model_type == "UNet_baseline":
         model = UNet_baseline()
+        dm = DataModuleSegmentation(path_to_train_source=hparams.source_data_path, path_to_train_target=hparams.target_data_path,
+            path_to_test=hparams.test_data_path, domain_adaptation=True, batch_size=hparams.batch_size, load_size=hparams.load_size)
     else:
         print("No correct model was chosen")
         raise SystemExit()
-
-    dm = DataModuleSegmentation(path_to_train_source=hparams.source_data_path, path_to_train_target=hparams.target_data_path,
-            path_to_test=hparams.test_data_path, domain_adaptation=True, batch_size=hparams.batch_size, load_size=hparams.load_size)
 
     checkpoint_callback = ModelCheckpoint(save_top_k=2, dirpath=hparams.checkpoint_dir, monitor="Overall Loss", mode="min") # saves top-K checkpoint based on metric defined with monitor
 
@@ -36,7 +43,7 @@ def main_FDA(hparams: Namespace):
 
 if __name__ == "__main__":
     parser = ArgumentParser(add_help=False)
-    parser.add_argument("--model_type", type=str, choices=["FDA", "UNet_baseline"], help="Name of the model which is used for training, FDA: Fourier Domain Adaptation")
+    parser.add_argument("--model_type", type=str, choices=["FDA", "FDA_self_supervised", "UNet_baseline"], help="Name of the model which is used for training, FDA: Fourier Domain Adaptation")
     parser.add_argument("--LB", type=float, default= 0.1, help="value (between (0,1)) of beta that examines how much of the amplitude of x^t replaces the amplitude of x^s. Refer to original paper for more information")
     parser.add_argument("--run_name", type=str, help="Name of the run in wandb")
     parser.add_argument("--project_name", type=str, help="Name of the project in wandb")
