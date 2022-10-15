f184b8c179a66871593f97b04a298a007ceb0b40
Lennart <LennartMa.19@gmail.com>
2022-10-15 17:53:34 +0200

    Update with normal ResNet model

--- models/main_model.py
+++ models/main_model.py
@@ -19,6 +19,8 @@ import torchvision.transforms.functional as F_vision
 
 import wandb
 
+from torchvision import models
+
 
 def visualize_feature_embedding_matplot(coarse_seg_mask, feature_embedding):
     """
@@ -55,16 +57,23 @@ def visualize_feature_embedding_matplot(coarse_seg_mask, feature_embedding):
     return fig
 
 
-def visualize_feature_embedding_torch(feature_embedding):
+def visualize_feature_embedding_torch(feature_embedding, feature_embed_prop, idx):
+    """
+    Visualizes the feaure embedding in a grid. 
+    feature_embedding (torch.tensor): (B, D, H*, W*) B:batch size, D:spatial feature dimension, H*,W*: height and width of the feature embedding
+    feature_embed_prop (float): The proportion of feature embeddings that is visualized in the grid. E.g. D=512 and feature_embed_prop=0.5
+        -> 256 feature embeddings are visualized in the grid
+    idx: (int): Which index of the B mini batches is visualized 
+    """
 
     # we split the feature embedding from size (B, D, H, W) into (B, D/4, H, W), because else the computation for visualization would take too long and lacking clarity
-    splited_embedding_size = int(feature_embedding.size()[1]/4)
-    splited_feature_embedding, _, _, _ = feature_embedding.split(splited_embedding_size, dim=1)
+    splited_embedding_size = int(feature_embedding.size()[1] * feature_embed_prop)
+    splited_feature_embedding_list = feature_embedding.split(splited_embedding_size, dim=1)
 
-    spatial_dim = splited_feature_embedding.size()[1]
+    spatial_dim = splited_feature_embedding_list[0].size()[1]
     grid_dim = math.ceil(math.sqrt(spatial_dim))
 
-    single_feature_embed = splited_feature_embedding[0]
+    single_feature_embed = splited_feature_embedding_list[idx][0]
     single_feature_embed = single_feature_embed.view(single_feature_embed.size()[0], 1, single_feature_embed.size()[1], single_feature_embed.size()[2])
 
     single_feature_embed_sigmoid = torch.sigmoid(single_feature_embed)
@@ -91,16 +100,16 @@ class Coarse_Decoder(nn.Module):
 
         elif coarse_decoder_type == "mlp":
             self.coarse_decoder = nn.Sequential(
-                nn.Conv2d(dim_in, dim_in/2, kernel_size=1, stride=1, padding=0),
+                nn.Conv2d(dim_in, 256, kernel_size=1, stride=1, padding=0),
                 nn.ReLU(inplace=True),
-                nn.Conv2d(dim_in/2, 1, kernel_size=1, stride=1, padding=0)
+                nn.Conv2d(256, dim_out, kernel_size=1, stride=1, padding=0)
             )
 
     def forward(self, x):
         return self.coarse_decoder(x)
 
 
-class Decoder(nn.Module):
+class Dilated_Decoder(nn.Module):
     def __init__(self, num_classes=1):
         super().__init__()
 
@@ -167,10 +176,86 @@ class Decoder(nn.Module):
         return segmentation_mask
 
 
+class Normal_Decoder(nn.Module):
+
+    def __init__(self, num_classes=1):
+        super().__init__()
+
+        self.num_classes = num_classes
+
+        self.bridge_layer0 = convrelu(64, 64, 1, 0)
+        self.bridge_layer1 = convrelu(64, 64, 1, 0)
+        self.bridge_layer2 = convrelu(128, 128, 1, 0)
+        self.bridge_layer3 = convrelu(256, 256, 1, 0)
+        self.bridge_layer4 = convrelu(512, 512, 1, 0)
+
+        self.conv_up_layer3 = convrelu(256 + 512, 512, 3, 1)
+        self.conv_up_layer2 = convrelu(128 + 512, 256, 3, 1)
+        self.conv_up_layer1 = convrelu(64 + 256, 256, 3, 1)
+        self.conv_up_layer0 = convrelu(64 + 256, 128, 3, 1)
+        self.conv_up_base_layer = convrelu(64 + 128, 64, 3, 1)
+
+        self.upsample_4_to_3 = nn.Upsample(size=(17, 17), mode='bilinear', align_corners=True)
+        self.upsample_3_to_2 = nn.Upsample(size=(33, 33), mode='bilinear', align_corners=True)
+        self.upsample_2_to_1 = nn.Upsample(size=(65, 65), mode='bilinear', align_corners=True)
+        self.upsample_1_to_0 = nn.Upsample(size=(128, 128), mode='bilinear', align_corners=True)
+        self.upsample_0_to_original = nn.Upsample(size=(256, 256), mode='bilinear', align_corners=True)
+
+        self.conv_layer_original_size0 = convrelu(3, 64, 3, 1)
+        self.conv_layer_original_size1 = convrelu(64, 64, 3, 1)
+
+        self.conv_last = nn.Conv2d(64, self.num_classes, 1)
+
+
+    def forward(self, input, layer0_output, layer1_output, layer2_output, layer3_output, layer4_output):
+
+        base_layer_output0 = self.conv_layer_original_size0(input)
+        base_layer_output1 = self.conv_layer_original_size1(base_layer_output0)
+
+        # layer4 to layer3
+        layer4 = self.bridge_layer4(layer4_output)
+        x = self.upsample_4_to_3(layer4)
+        layer3 = self.bridge_layer3(layer3_output)
+        x = torch.cat([x, layer3], dim=1)
+        x = self.conv_up_layer3(x)
+
+        # layer3 to layer2
+        x = self.upsample_3_to_2(x)
+        layer2 = self.bridge_layer2(layer2_output)
+        x = torch.cat([x, layer2], dim=1)
+        x = self.conv_up_layer2(x)
+
+        # layer2 to layer1
+        x = self.upsample_2_to_1(x)
+        layer1 = self.bridge_layer1(layer1_output)
+        x = torch.cat([x, layer1], dim=1)
+        x = self.conv_up_layer1(x)
+
+        # layer1 to layer0
+        x = self.upsample_1_to_0(x)
+        layer0 = self.bridge_layer0(layer0_output)
+        x = torch.cat([x, layer0], dim=1)
+        x = self.conv_up_layer0(x)
+
+        # layer0 to original_layer
+        x = self.upsample_0_to_original(x)
+        x = torch.cat([x, base_layer_output1], dim=1)
+        x = self.conv_up_base_layer(x)
+
+        # original_layer to output
+        segmentation_mask = self.conv_last(x)
+
+        return segmentation_mask
+
+
 
 class MainNetwork(pl.LightningModule):
     def __init__(
                 self,
+                index_range, # defines the indices (range from first idx to last idx) that are used for logging/visualizing seg_masks/predictions/feature_maps in a mini batch during validation
+                model_type,
+                coarse_prediction_type,
+                coarse_lambda,
                 lr=1e-3
     ):
         super().__init__()
@@ -180,9 +265,34 @@ class MainNetwork(pl.LightningModule):
         self.seg_metric = Dice()
         
         self.resnet_backbones = ResNetBackbone()
-        self.encoder = self.resnet_backbones.deepbase_resnet18_dilated8()
-        self.coarse_decoder = Coarse_Decoder(self.encoder.num_features)
-        self.decoder = Decoder()
+
+        if model_type == "dilated":
+            print("Using a dilated ResNet18 backbone", "\n")
+            self.encoder = self.resnet_backbones.deepbase_resnet18_dilated8()
+            self.decoder = Dilated_Decoder()
+
+        elif model_type == "normal":
+            print("Using a normal ResNet18 backbone", "\n")
+            self.encoder = self.resnet_backbones.resnet18()
+            self.decoder = Normal_Decoder()
+
+        # Setting the Coarse Prediction type (What is the architecture of the coarse prediction head)
+        if coarse_prediction_type == "linear":
+            print("Using a coarse Decoder with linear conv layer", "\n")
+            self.coarse_decoder = Coarse_Decoder(self.encoder.num_features, coarse_decoder_type="linear")
+
+        elif coarse_prediction_type == "mlp":
+            print("Using a coarse Decoder with mlp layers", "\n")
+            self.coarse_decoder = Coarse_Decoder(self.encoder.num_features, coarse_decoder_type="mlp")
+
+        else:
+            print("Using no coarse Decoder", "\n")
+
+        self.coarse_prediction_type = coarse_prediction_type
+        self.coarse_lambda = coarse_lambda
+
+        self.index_range = list(range(index_range[0],index_range[1]+1))
+
 
     def forward(self, input_data):
 
@@ -190,11 +300,16 @@ class MainNetwork(pl.LightningModule):
 
         layer0_output, maxpool_output, layer1_output, layer2_output, layer3_output, layer4_output = output
 
-        coarse_prediction = self.coarse_decoder(layer4_output)
+        if self.coarse_prediction_type == "no_coarse":
+            coarse_prediction = None
+
+        else:    
+            coarse_prediction = self.coarse_decoder(layer4_output)
 
         segmentation_mask_prediction = self.decoder(input_data, layer0_output, layer1_output, layer2_output, layer3_output, layer4_output)
 
         return segmentation_mask_prediction, coarse_prediction, layer4_output
+
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -210,12 +325,14 @@ class MainNetwork(pl.LightningModule):
 
         segmentation_mask_prediction, coarse_output, _ = self(img)
 
-        coarse_loss = F.binary_cross_entropy_with_logits(coarse_output, mask_coarse)
-
         loss = F.binary_cross_entropy_with_logits(segmentation_mask_prediction, mask)
 
-        self.log("Training Coarse Loss (Binary Cross Entropy)", coarse_loss, prog_bar=True)
         self.log("Training Loss (Binary Cross Entropy)", loss, prog_bar=True)
+
+        if self.coarse_prediction_type != "no_coarse":
+            coarse_loss = F.binary_cross_entropy_with_logits(coarse_output, mask_coarse)
+            self.log("Training Coarse Loss (Binary Cross Entropy)", coarse_loss, prog_bar=True)
+            loss = loss + self.coarse_lambda * coarse_loss
 
         return loss
 
@@ -226,61 +343,80 @@ class MainNetwork(pl.LightningModule):
         segmentation_mask_prediction, coarse_output, feature_embedding = self(img)
 
         if batch_idx == 0 and self.current_epoch % 4 == 0:
-            
-            # Get the feature embedding (normal and sigmoid) as wandb.Image for logging
-            grid_array, grid_array_sigmoid = visualize_feature_embedding_torch(feature_embedding)
-            grid_array = wandb.Image(grid_array, caption="Feature Embedding")
-            grid_array_sigmoid = wandb.Image(grid_array_sigmoid, caption="Feature Embedding Sigmoid")
 
-            # Get the true Segmentation Mask as wandb.Image for logging
-            coarse_seg_mask = wandb.Image(F_vision.to_pil_image(mask_coarse[0][0]).convert("L"))
-            seg_mask = wandb.Image(F_vision.to_pil_image(mask[0][0]).convert("L"))
-            
-            # Get the coarse prediction output (normal and sigmoid) as wandb.Image for logging
-            coarse_output_image = wandb.Image(F_vision.to_pil_image(coarse_output[0][0]).convert("L"))
-            coarse_output_sigmoid = torch.sigmoid(coarse_output)
-            coarse_output_image_sigmoid = wandb.Image(F_vision.to_pil_image(coarse_output_sigmoid[0][0]).convert("L"))
+            for idx in self.index_range:
 
-            # Get the prediction output (normal and sigmoid) as wandb.Image for logging
-            output_image = wandb.Image(F_vision.to_pil_image(segmentation_mask_prediction[0][0]).convert("L"))
-            output_sigmoid = torch.sigmoid(segmentation_mask_prediction)
-            output_image_sigmoid = wandb.Image(F_vision.to_pil_image(output_sigmoid[0][0]).convert("L"))
-            
+                # Get the feature embedding (normal and sigmoid) as wandb.Image for logging
+                grid_array, grid_array_sigmoid = visualize_feature_embedding_torch(feature_embedding, feature_embed_prop=0.5, idx=idx)
+                grid_array = wandb.Image(grid_array)
+                grid_array_sigmoid = wandb.Image(grid_array_sigmoid)
 
-            wandb.log({
-                    f"Feature Embedding": grid_array,
-                    f"Feature Embedding Sigmoid": grid_array_sigmoid,
-                    f"Coarse True Segmentation Mask": coarse_seg_mask,
-                    f"True Segmentation Mask": seg_mask,
-                    f"Coarse Prediction Output ": coarse_output_image,
-                    f"Coarse Prediction Output Sigmoid": coarse_output_image_sigmoid,
-                    f"Prediction Output ": output_image,
-                    f"Prediction Output Sigmoid": output_image_sigmoid,
-                    })
+                # Get the true Segmentation Mask as wandb.Image for logging
+                coarse_seg_mask = wandb.Image(F_vision.to_pil_image(mask_coarse[idx][0]).convert("L"))
+                seg_mask = wandb.Image(F_vision.to_pil_image(mask[idx][0]).convert("L"))
+                
+                # Get the prediction output (normal and sigmoid) as wandb.Image for logging
+                # output_image = wandb.Image(F_vision.to_pil_image(segmentation_mask_prediction[idx][0]).convert("L"))
+                output_sigmoid = torch.sigmoid(segmentation_mask_prediction)
+                output_segmap_sigmoid = wandb.Image(F_vision.to_pil_image(output_sigmoid[idx][0]).convert("L"))
 
-        coarse_val_loss = F.binary_cross_entropy_with_logits(coarse_output, mask_coarse)
-        val_loss = F.binary_cross_entropy_with_logits(segmentation_mask_prediction, mask)
+                # Get the true images for logging
+                input_image = wandb.Image(F_vision.to_pil_image(img[idx]))
+
+
+                if self.coarse_prediction_type != "no_coarse":
+                    # Get the coarse prediction output (normal and sigmoid) as wandb.Image for logging
+                    coarse_output_image = wandb.Image(F_vision.to_pil_image(coarse_output[idx][0]).convert("L"))
+                    coarse_output_sigmoid = torch.sigmoid(coarse_output)
+                    coarse_output_segmap_sigmoid = wandb.Image(F_vision.to_pil_image(coarse_output_sigmoid[idx][0]).convert("L"))
+    
+                    wandb.log({
+                            # f"Feature Embedding {idx}": grid_array,
+                            f"Feature Embedding Sigmoid {idx}": grid_array_sigmoid,
+                            f"Coarse True Segmentation Mask {idx}": coarse_seg_mask,
+                            f"True Segmentation Mask {idx}": seg_mask,
+                            # f"Coarse Prediction Output {idx}": coarse_output_image,
+                            f"Coarse Prediction Output Sigmoid {idx}": coarse_output_segmap_sigmoid,
+                            # f"Prediction Output {idx}": output_image,
+                            f"Prediction Output Sigmoid {idx}": output_segmap_sigmoid,
+                            f"Input image {idx}": input_image
+                            })
+                else:
+                    wandb.log({
+                            # f"Feature Embedding {idx}": grid_array,
+                            f"Feature Embedding Sigmoid {idx}": grid_array_sigmoid,
+                            f"Coarse True Segmentation Mask {idx}": coarse_seg_mask,
+                            f"True Segmentation Mask {idx}": seg_mask,
+                            # f"Prediction Output {idx}": output_image,
+                            f"Prediction Output Sigmoid {idx}": output_segmap_sigmoid,
+                            f"Input image {idx}": input_image
+                            })
+
         
-        self.coarse_seg_metric.update(coarse_output, mask_coarse.to(dtype=torch.uint8))
-
+        val_loss = F.binary_cross_entropy_with_logits(segmentation_mask_prediction, mask)
         self.seg_metric.update(segmentation_mask_prediction, mask.to(dtype=torch.uint8))
+        self.log("Validation Loss (BCE)", val_loss, on_step=False, on_epoch=True)
 
-        return {"coarse_vall_loss": coarse_val_loss, "val_loss": val_loss}
+        if self.coarse_prediction_type != "no_coarse":
+            coarse_val_loss = F.binary_cross_entropy_with_logits(coarse_output, mask_coarse)
+            self.coarse_seg_metric.update(coarse_output, mask_coarse.to(dtype=torch.uint8))
+            return {"coarse_vall_loss": coarse_val_loss, "val_loss": val_loss}
+        
+        else:
+            return {"val_loss": val_loss}
 
     def validation_epoch_end(self, outs):
         # outs is a list of whatever you returned in `validation_step`
 
-        coarse_loss = torch.stack([outs[0]["coarse_vall_loss"]]).mean()
-        coarse_dice = self.coarse_seg_metric.compute()
+        if self.coarse_prediction_type != "no_coarse":
+            # coarse_loss = torch.stack([outs[0]["coarse_vall_loss"]]).mean()
+            coarse_dice = self.coarse_seg_metric.compute()
+            # self.log("Validation Coarse Loss (Binary Cross Entropy)", coarse_loss, prog_bar=True)
+            self.log("Coarse Dice Score (Validation)", coarse_dice, prog_bar=True)
+            self.coarse_seg_metric.reset()
 
-        self.log("Validation Coarse Loss (Binary Cross Entropy)", coarse_loss, prog_bar=True)
-        self.log("Coarse Dice Score (Validation)", coarse_dice, prog_bar=True)
-
-        loss = torch.stack([outs[0]["val_loss"]]).mean()
+        # loss = torch.stack([outs[0]["val_loss"]]).mean()
         dice = self.seg_metric.compute()
-
-        self.log("Loss (Binary Cross Entropy)", loss, prog_bar=True)
+        # self.log("Validation Loss (Binary Cross Entropy)", loss, prog_bar=True)
         self.log("Dice Score (Validation)", dice, prog_bar=True)
-
-        self.coarse_seg_metric.reset()
         self.seg_metric.reset()
--- trainer_main_model.py
+++ trainer_main_model.py
@@ -12,7 +12,7 @@ from utils.data import DataModuleSegmentation
 import wandb
 
 
-def main(hparams: Namespace, argparse_dict):
+def main(hparams: Namespace):
 
     dm = DataModuleSegmentation(path_to_train_source=hparams.path_to_train, load_size=256, coarse_segmentation=33, num_workers=hparams.num_workers)
 
@@ -20,14 +20,12 @@ def main(hparams: Namespace, argparse_dict):
 
     wandb_logger = WandbLogger(name=hparams.run_name, project=hparams.project_name, log_model="True")
 
-    model = MainNetwork()
+    model = MainNetwork(index_range=hparams.index_range, model_type=hparams.model_type, coarse_prediction_type=hparams.coarse_prediction_type, coarse_lambda=hparams.coarse_lambda)
 
     trainer = pl.Trainer(callbacks=checkpoint_callback, accelerator=hparams.device, devices=1, logger=wandb_logger, max_epochs=hparams.max_epochs,
                         fast_dev_run=hparams.debug)
 
     wandb_logger.watch(model)
-
-    wandb.init(config=argparse_dict)
 
     trainer.fit(model, dm)
 
@@ -42,7 +40,12 @@ if __name__ == "__main__":
     parser.add_argument("--project_name", type=str, help="Name of the project in wandb")
     parser.add_argument("--checkpoint_dir", type=str, help="path where the checkpoint (model etc.) should be saved")
     parser.add_argument("--path_to_train", type=str, help="path where the training data is stored")
+    parser.add_argument("--model_type", type=str, choices=["normal", "dilated"] , help="What type of model is used, either normal or dilated ResNet18")
+    parser.add_argument("--coarse_prediction_type", type=str, choices=["no_coarse", "linear", "mlp"], help="which type of coarse prediction should be used")
 
+    # All arguments with default arguments
+    parser.add_argument("--coarse_lambda", type=float, default=1.0, help="Coefficient used for the coarse loss in the overall loss")
+    parser.add_argument("--index_range", type=list, default=[0,1], help="defines the indices (range from first idx to last idx) that are used for logging/visualizing seg_masks/predictions/feature_maps in a mini batch during validation")
     parser.add_argument("--load_size", type=int, default=256, help="size to which images will get resized")
     parser.add_argument("--test_data_path", default=None, type=str, help="path where the test target dataset is stored")
     parser.add_argument("--max_epochs", default=150, type=int, help="maximum epochs for training")
@@ -68,4 +71,4 @@ if __name__ == "__main__":
         # Create a namespace from the argparse_dict
         hparams = Namespace(**argparse_dict)
 
-    main(hparams, argparse_dict)
+    main(hparams)
