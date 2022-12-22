a2023f125cae1ab3c0c85e788f349eb2a9cf2e19
lennart-maack <lennartma.19@gmail.com>
2022-12-22 21:39:53 +0100

    Use less augs

--- main_method/utils/data.py
+++ main_method/utils/data.py
@@ -221,7 +221,7 @@ class CustomDataset(torch.utils.data.Dataset):
                 A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, brightness_by_max=True, p=0.3),
                 # A.HueSaturationValue(p=0.4),
                 A.Flip(p=0.5),
-                A.Rotate(p=0.3),
+                # A.Rotate(p=0.3),
                 # A.ElasticTransform(p=0.4, alpha_affine=15, interpolation=1, border_mode=0, value=0, mask_value=0),
                 # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                 ToTensorV2(),
